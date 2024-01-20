8f99039d5a24ca8448db486856b8020789a59e00
Leonardo Amaral <contato@leonardoamaral.com.br>
2024-01-19 23:56:29 -0300

    Added DNSMASQ_USE_RESOLV and DNSMASQ_DHCP_DNS

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+if [ ! "${DNSMASQ_USE_RESOLV}" = true ]; then DNSMASQ_USE_RESOLV_OPTION="no-resolv"; DNSMASQ_USE_POLL_OPTION="no-poll"; fi
+
 if [ "${DNSSEC}" = true ] ; then
     DNSMASQ_DNSSEC="dnssec"
     if [ "${DNSMASQ_DNSSEC_CHECK_UNSIGNED}" = true ]; then DNSMASQ_DNSSEC_CHECK_UNSIGNED_OPTION="dnssec-check-unsigned"; fi
@@ -13,8 +15,10 @@ if [ "${DHCP}" = true ] ; then
     if [ -z ${DNSMASQ_DHCP_END_RANGE+x} ]; then echo "DNSMASQ_DHCP_END_RANGE is not set. Exiting"; exit 1 ; fi
     if [ -z ${DNSMASQ_DHCP_NETMASK_RANGE+x} ]; then echo "DNSMASQ_DHCP_NETMASK_RANGE is not set. Exiting"; exit 1 ; fi
     if [ -z ${DNSMASQ_DHCP_GATEWAY+x} ]; then echo "DNSMASQ_DHCP_GATEWAY is not set. Exiting"; exit 1 ; fi
+    if [ -z ${DNSMASQ_DHCP_DNS+x} ]; then echo "DNSMASQ_DHCP_DNS is not set. Exiting"; exit 1 ; fi
     DNSMASQ_DHCP_RANGE="dhcp-range=${DNSMASQ_DHCP_START_RANGE},${DNSMASQ_DHCP_END_RANGE},${DNSMASQ_DHCP_NETMASK_RANGE},${DNSMASQ_DHCP_LEASE_TIME:-12h}"
     DNSMASQ_DHCP_ROUTER="dhcp-option=option:router,${DNSMASQ_DHCP_GATEWAY}"
+    DNSMASQ_DHCP_DNS_OPTION="dhcp-option=6,${DNSMASQ_DHCP_DNS}"
     if [ ! -z ${DNSMASQ_DHCP_NTP+x} ]; then DNSMASQ_DHCP_NTP_OPTION="dhcp-option=option:ntp-server,${DNSMASQ_DHCP_NTP}" ; fi
     if [ ! -z ${DNSMASQ_DHCP_WPAD+x} ]; then DNSMASQ_DHCP_WPAD_OPTION="dhcp-option=252,\"${DNSMASQ_DHCP_WPAD}\"" ; else DNSMASQ_DHCP_WPAD_OPTION="dhcp-option=252,\"\n\"" ; fi
     DNSMASQ_DHCP_MS_RELEASE="dhcp-option=vendor:MSFT,2,1i"
@@ -37,9 +41,8 @@ domain=${DNS_LOCAL_DOMAIN:-local}
 domain-needed
 bogus-priv
 no-hosts
-no-resolv
-no-poll
-except-interface=nonexisting
+${DNSMASQ_USE_RESOLV_OPTION:-}
+${DNSMASQ_USE_POLL_OPTION:-}
 log-facility=-
 dhcp-leasefile=/var/lib/misc/dnsmasq.leases
 dhcp-name-match=set:wpad-ignore,wpad
@@ -49,6 +52,7 @@ no-negcache
 rebind-domain-ok=${DNS_LOCAL_DOMAIN:-local}
 ${DNSMASQ_DHCP_RANGE:-}
 ${DNSMASQ_DHCP_ROUTER:-}
+${DNSMASQ_DHCP_DNS_OPTION:-}
 ${DNSMASQ_DHCP_NTP_OPTION:-}
 ${DNSMASQ_DHCP_WPAD_OPTION:-}
 ${DNSMASQ_DHCP_MS_RELEASE:-}
