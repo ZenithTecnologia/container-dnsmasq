558b089507ce7cdba8a1db7b104999091d044fd1
Leonardo Amaral <contato@leonardoamaral.com.br>
2024-01-19 22:08:20 -0300

    Wrong dhcp-lease-max declaration

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,8 @@ if [ "${DHCP}" = true ] ; then
     DNSMASQ_DHCP_ROUTER="dhcp-option=option:router,${DNSMASQ_DHCP_GATEWAY}"
     if [ ! -z ${DNSMASQ_DHCP_NTP+x} ]; then DNSMASQ_DHCP_NTP_OPTION="dhcp-option=option:ntp-server,${DNSMASQ_DHCP_NTP}" ; fi
     if [ ! -z ${DNSMASQ_DHCP_WPAD+x} ]; then DNSMASQ_DHCP_WPAD_OPTION="dhcp-option=252,\"${DNSMASQ_DHCP_WPAD}\"" ; else DNSMASQ_DHCP_WPAD_OPTION="dhcp-option=252,\"\n\"" ; fi
-    DNSMASQ_DHCP_MS_RELEASE='dhcp-option=vendor:MSFT,2,1i'
-    DNSMASQ_MAX_LEASE_OPTION=${DNSMASQ_MAX_LEASE_OPTION:-150}
+    DNSMASQ_DHCP_MS_RELEASE="dhcp-option=vendor:MSFT,2,1i"
+    DNSMASQ_MAX_LEASE_OPTION="dhcp-lease-max=${DNSMASQ_MAX_LEASE_OPTION:-150}"
     if [ "${DNSMASQ_RAPID_COMMIT}" = true ] ; then
         DNSMASQ_RAPID_COMMIT_OPTION="dhcp-rapid-commit"
     fi
