aac2ce1a697a6b2600e139ef6f6d88b682b173e0
Leonardo Amaral <contato@leonardoamaral.com.br>
2024-01-19 21:48:22 -0300

    Fix typo #3

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ if [ "${DHCP}" = true ] ; then
     DNSMASQ_DHCP_RANGE="dhcp-range=${DNSMASQ_DHCP_START_RANGE},${DNSMASQ_DHCP_END_RANGE},${DNSMASQ_DHCP_NETMASK_RANGE},${DNSMASQ_DHCP_LEASE_TIME:-12h}"
     DNSMASQ_DHCP_ROUTER="dhcp-option=option:router,${DNSMASQ_DHCP_GATEWAY}"
     if [ ! -z ${DNSMASQ_DHCP_NTP+x} ]; then DNSMASQ_DHCP_NTP_OPTION="dhcp-option=option:ntp-server,${DNSMASQ_DHCP_NTP}" ; fi
-    if [ ! -z ${DNSMASQ_DHCP_WPAD+x} ]; then DNSMASQ_DHCP_WPAD_OPTION="dhcp-option=option:252,${DNSMASQ_DHCP_WPAD}" ; else DNSMASQ_DHCP_WPAD_OPTION="dhcp-option=option:252,\"\n\"" ; fi
+    if [ ! -z ${DNSMASQ_DHCP_WPAD+x} ]; then DNSMASQ_DHCP_WPAD_OPTION="dhcp-option=option:252,\"${DNSMASQ_DHCP_WPAD}\"" ; else DNSMASQ_DHCP_WPAD_OPTION="dhcp-option=option:252,\"\n\"" ; fi
     DNSMASQ_DHCP_MS_RELEASE='dhcp-option=vendor:MSFT,2,1i'
     DNSMASQ_MAX_LEASE_OPTION=${DNSMASQ_MAX_LEASE_OPTION:-150}
     if [ "${DNSMASQ_RAPID_COMMIT}" = true ] ; then
