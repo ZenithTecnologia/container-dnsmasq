49fb13dd7b6513c604ad7daf9d1cecab535e8691
Leonardo Amaral <contato@leonardoamaral.com.br>
2024-01-20 21:13:32 -0300

    Fix on multiple servers

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 if [ ! "${DNSMASQ_USE_RESOLV}" = true ]; then 
     DNSMASQ_USE_RESOLV_OPTION="no-resolv"; DNSMASQ_USE_POLL_OPTION="no-poll"
     for dnsserver in ${DNS_FORWARD_SERVER//,/ }; do
-	    DNSMASQ_USE_RESOLV_OPTION=${dnsserver}
+	    DNSMASQ_USE_RESOLV_OPTION=server=${dnsserver}
 	    DNSMASQ_USE_RESOLV_OPTION="${DNSMASQ_USE_RESOLV_OPTION}
-	    ${dnsserver}"
+server=${dnsserver}"
     done
     DNS_FORWARD_SERVER_OPTION="server=${DNS_FORWARD_SERVER:-8.8.8.8}"
 else
